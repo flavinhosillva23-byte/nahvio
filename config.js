2d64c58dea7b42f06c45172ba41223f9e8beaae8
flavinhosillva23-byte <flavinhosillva23@gmail.com>
2026-07-22 17:47:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Cole abaixo os dados encontrados em:
+// Supabase > Project Settings > API
+window.WEDDING_CONFIG = {
+  supabaseUrl: "COLE_AQUI_A_PROJECT_URL",
+  supabaseAnonKey: "COLE_AQUI_A_ANON_PUBLIC_KEY"
+};

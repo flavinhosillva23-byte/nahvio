968202c069e190456950cb6d1d161091ab62ad3b
flavinhosillva23-byte <flavinhosillva23@gmail.com>
2026-07-22 17:59:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Cole abaixo os dados encontrados em:
 // Supabase > Project Settings > API
 window.WEDDING_CONFIG = {
-  supabaseUrl: "COLE_AQUI_A_PROJECT_URL",
-  supabaseAnonKey: "COLE_AQUI_A_ANON_PUBLIC_KEY"
+  supabaseUrl: "https://slxckjqyjntdkkelgkvs.supabase.co",
+  supabaseAnonKey: "sb_publishable_XT3JVziJ50OwL3LP7zTzBA_7jHA5UzX"
 };
